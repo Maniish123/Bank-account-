116dd31f703bd4fc7ec619d8669956278b517a81
Maniish123 <132150882+Maniish123@users.noreply.github.com>
2025-04-08 23:05:50 +0530

    Add files via upload

---
+++ TransactionDto.java
@@ -0,0 +1,18 @@
+package com.example.Bank_management_system.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class TransactionDto {
+    private String transactionType;
+    private BigDecimal amount;
+    private String accountNumber;
+    private String status;
+}
